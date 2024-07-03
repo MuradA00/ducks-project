031fb6ea3629f03e9f92fe387ac02fdcd95900d7
MuradA00 <murad.abyshly@cowchain.io>
2024-07-04 00:46:17 +0300

    90%

--- source/js/main.js
+++ source/js/main.js
@@ -6,14 +6,60 @@ import './_vendor';
 const accsItems = document.querySelectorAll('.faq-list__item');
 const mobNav = document.querySelector('.header-nav');
 const panelMenu = document.querySelector('.menu-burger');
+const winModal = document.querySelector('#win-modal')
+const winButton = document.querySelector('#win-button');
 const roulettePanelButtons = document.querySelectorAll('.roulette-toggle');
+const soundToggler = document.querySelector('.heading-sound')
 const body = document.body;
+const upgradeTabsButtons = document.querySelectorAll('.upgrade-tabs__item');
 
-roulettePanelButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.classList.remove('roulette-toggle--active');
+const closeModalByOuterClick = (modal) => {
+  const modalContainer = modal.querySelector('.modal-inner');
+
+  modalContainer.addEventListener('click', (e) => {
+    if (e.target === modalContainer) {
+      hideModal();
+      modal.classList.remove('modal--active');
+    }
   })
-});
+}
+
+const hideModal = () => {
+  body.classList.remove('locked')
+  document.documentElement.classList.remove('locked')
+}
+
+const showModal = () => {
+  body.classList.add('locked');
+  document.documentElement.classList.add('locked');
+}
+
+if (soundToggler) {
+  soundToggler.addEventListener('click', () => soundToggler.classList.toggle('heading-sound--disabled'))
+}
+
+if (winButton) {
+  winButton.addEventListener('click', () => {
+    winModal.classList.add('modal--active');
+
+    closeModalByOuterClick(winModal);
+    showModal();
+  })
+}
+
+const handleToggleItems = (array, activeClass) => {
+  if (array.length > 0) {
+    array.forEach(item => {
+      item.addEventListener('click', () => {
+        array.forEach(item => item.classList.remove(activeClass));
+        item.classList.add(activeClass);
+      })
+    })
+  }
+}
+
+handleToggleItems(upgradeTabsButtons, 'upgrade-tabs__item--active');
+handleToggleItems(roulettePanelButtons, 'roulette-toggle--active');
 
 const menuHandler = () => {
   panelMenu.classList.toggle('menu-burger--active');
